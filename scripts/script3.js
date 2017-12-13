7082cdae68a4b7d7d70799a9eab850cf5a595494
michaelao77 <31858611+michaelao77@users.noreply.github.com>
2017-12-12 23:20:04 -0500

    Add files via upload

---
+++ scripts/script3.js
@@ -0,0 +1,25 @@
+// P1
+function P1() {
+    document.getElementById("P1").style.display = "block";
+}
+
+function noP1() {
+    document.getElementById("P1").style.display = "none";
+}
+
+// A3
+function A3() {
+	document.getElementById("A3b").style.display = "block";
+}
+function noA3() {
+    document.getElementById("A3b").style.display = "none";
+}
+
+
+// A7
+function A7() {
+	document.getElementById("A7").style.display = "block";
+}
+function noA7() {
+    document.getElementById("A7").style.display = "none";
+}
